f0438595d334189b7d02849fd8ebffc8072156ed
Young998904 <like0998904@gmail.com>
2022-07-15 17:50:40 +0900

    목록 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,11 +1,15 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
+    List<WiseSaying> wiseSayings;
     public App (Scanner sc) {
         this.sc = sc;
+        wiseSayings = new ArrayList<>();
     }
 
     public void run() {
@@ -27,8 +31,17 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
 
+                    wiseSayings.add(new WiseSaying(id, content, author));
+
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
+                case "목록" :
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+                    for (WiseSaying wiseSaying : wiseSayings) {
+                        System.out.printf("%d / %s / %s\n",
+                                wiseSaying.id, wiseSaying.author, wiseSaying.content);
+                    }
             }
         }
     }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+    // 등록기능 구현 시작
     @Test
     public void 등록을_하면_명언과_작가를_물어본다() {
         String rs = AppTestRunner.run("""
@@ -42,7 +43,7 @@ public class AppTest {
     public void 등록을_하면_생성된_명언의_번호가_출력되어야_한다() {
         String rs = AppTestRunner.run("""
                 등록
-                나의 죽음을 적들에게 알리지 말라
+                나의 죽음을 적들에게 알리지 말라.
                 이순신
                 등록
                 나에게 불가능이란 없다.
@@ -52,6 +53,27 @@ public class AppTest {
 
         assertTrue(rs.contains("1번 명언이 등록되었습니다."));
         assertTrue(rs.contains("2번 명언이 등록되었습니다."));
+    }
+    // 등록 기능 구현 종료
+
+    // 목록 기능 구현 시작
+    @Test
+    public void 등록_후_목록에서_확인할_수_있어야_한다() {
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라.
+                이순신
+                등록
+                나에게 불가능이란 없다.
+                나폴레옹
+                목록
+                종료
+                """);
+
+        assertTrue(rs.contains("번호 / 작가 / 명언"));
+        assertTrue(rs.contains("----------------------"));
+        assertTrue(rs.contains("2 / 나폴레옹 / 나에게 불가능이란 없다."));
+        assertTrue(rs.contains("1 / 이순신 / 나의 죽음을 적들에게 알리지 말라."));
     }
 
     @Test
